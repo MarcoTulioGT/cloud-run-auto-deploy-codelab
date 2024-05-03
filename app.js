9de8cda90533ca44b7015b1404220475c703b82c
Marco <devopsgt@gmail.com>
2024-05-02 18:44:00 -0600

    fixed town bug

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ app.post("/update", async function (req, res) {
     let town = req.body.town;
     const doc = firestoreDb.doc(`demo/${name}`);
 
-    //TODO: fix this bug
+    //fixed town bug
     await doc.set({
-        name: name
-        /* town: town */
+        name: name,
+        town: town
     });
 
     res.send(`<div hx-target="this" hx-swap="outerHTML" hx-indicator="spinner">
